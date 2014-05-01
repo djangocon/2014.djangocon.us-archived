3f8194c248ff0f83e64cb827f69561e0a787972f
Trevor Ray <tray@caktusgroup.com>
2014-05-01 15:15:12 -0400

    uncommented code to activate real site templates.

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -17,32 +17,34 @@ if 'comps' in settings.INSTALLED_APPS:
     urlpatterns += patterns('', url(r'^', include('comps.urls')))
 urlpatterns += patterns("",
     url(r"^$",
-        TemplateView.as_view(template_name="splash.html"),
+        # TemplateView.as_view(template_name="splash.html"),
+        TemplateView.as_view(template_name="homepage.html"),
         name="home"),
     url(r"^admin/", include(admin.site.urls)),
 
 
-    # url(r"^registration/$",
-    #     TemplateView.as_view(template_name="registration.html"),
-    #     name="registration"),
+    url(r"^registration/$",
+        TemplateView.as_view(template_name="registration.html"),
+        name="registration"),
 
     url(r"^account/signup/$", symposion.views.SignupView.as_view(), name="account_signup"),
     url(r"^account/login/$", symposion.views.LoginView.as_view(), name="account_login"),
     url(r"^account/", include("account.urls")),
 
-    # url(r"^schedule/json/$", djangocon.views.schedule_json, name="schedule_json"),
+    url(r"^schedule/json/$", djangocon.views.schedule_json, name="schedule_json"),
 
-    # url(r"^dashboard/", symposion.views.dashboard, name="dashboard"),
-    # url(r"^speaker/", include("symposion.speakers.urls")),
-    # url(r"^proposals/", include("symposion.proposals.urls")),
-    # url(r"^sponsors/", include("symposion.sponsorship.urls")),
-    # url(r"^boxes/", include("symposion.boxes.urls")),
-    # url(r"^teams/", include("symposion.teams.urls")),
-    # url(r"^reviews/", include("symposion.reviews.urls")),
-    # url(r"^schedule/", include("symposion.schedule.urls")),
-    # url(r"^markitup/", include("markitup.urls")),
+    url(r"^blog/", include("biblion.urls")),
+    url(r"^dashboard/", symposion.views.dashboard, name="dashboard"),
+    url(r"^speaker/", include("symposion.speakers.urls")),
+    url(r"^proposals/", include("symposion.proposals.urls")),
+    url(r"^sponsors/", include("symposion.sponsorship.urls")),
+    url(r"^boxes/", include("symposion.boxes.urls")),
+    url(r"^teams/", include("symposion.teams.urls")),
+    url(r"^reviews/", include("symposion.reviews.urls")),
+    url(r"^schedule/", include("symposion.schedule.urls")),
+    url(r"^markitup/", include("markitup.urls")),
 
-    # url(r"^", include("symposion.cms.urls")),
+    url(r"^", include("symposion.cms.urls")),
 )
 
 
