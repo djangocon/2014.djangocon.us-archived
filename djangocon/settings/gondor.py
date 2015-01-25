cf3fe11c07484a2928fa88841ac3baa2ea828b9e
Jeff Triplett <jeff.triplett@gmail.com>
2015-01-25 11:42:32 -0600

    Updated allowed_hosts

--- djangocon/settings/gondor.py
+++ djangocon/settings/gondor.py
@@ -8,8 +8,7 @@ DEBUG = {"dev": True}.get(os.environ["GONDOR_INSTANCE"], False)
 ALLOWED_HOSTS = [
     'tk551.gondor.co',
     'fi290.gondor.co',
-    'djangocon.us',
-    'www.djangocon.us'
+    '2014.djangocon.us',
 ]
 
 if "GONDOR_DATABASE_URL" in os.environ:
